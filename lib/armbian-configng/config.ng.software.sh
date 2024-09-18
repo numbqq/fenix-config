fbde1e47ccbb6a64ff17d36293907acb8deb3022
Gunjan Gupta <gunjan@wesion.com>
2024-09-18 10:31:02 +0530

    Fix spacing and add option to install docker
    Signed-off-by: Gunjan Gupta <gunjan@wesion.com>

--- lib/armbian-configng/config.ng.software.sh
+++ lib/armbian-configng/config.ng.software.sh
@@ -8,7 +8,7 @@ module_options+=(
     ["see_monitoring,doc_link"]=""
 )
 #
-# @decrition generate a menu for armbianmonitor
+# @decription generate a menu for armbianmonitor
 #
 function see_monitoring() {
   if [ -f /usr/bin/htop ]; then
@@ -18,5 +18,58 @@ function see_monitoring() {
      
   else
     echo "htop is not installed"
+  fi
+}
+
+
+module_options+=(
+    ["install_docker,author"]="Gunjan Gupta"
+    ["install_docker,ref_link"]=""
+    ["install_docker,feature"]="install docker"
+    ["install_docker,desc"]="Install docker"
+    ["install_docker,example"]="install_docker"
+    ["install_docker,status"]="review"
+    ["install_docker,doc_link"]=""
+)
+#
+# @decription install docker
+#
+install_docker ()
+{
+	if wget -q -t 1 --timeout=5 --spider https://download.docker.com/linux/${DISTRO,,}/dists/${DISTROID}/stable ; then
+		curl -fsSL https://download.docker.com/linux/${DISTRO,,}/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+		echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/${DISTRO,,} ${DISTROID} stable" \
+			> /etc/apt/sources.list.d/docker.list
+		debconf-apt-progress -- apt-get update
+		debconf-apt-progress -- apt-get install -y -qq docker-ce
+	else
+		debconf-apt-progress -- apt-get update
+		debconf-apt-progress -- apt-get install -y -qq docker.io
+	fi
+
+  if [ -n "$SUDO_USER" ]; then
+    usermod -aG docker $SUDO_USER
+  fi
+}
+
+
+module_options+=(
+    ["remove_docker,author"]="Gunjan Gupta"
+    ["remove_docker,ref_link"]=""
+    ["remove_docker,feature"]="remove docker"
+    ["remove_docker,desc"]="Remove docker"
+    ["remove_docker,example"]="remove_docker"
+    ["remove_docker,status"]="review"
+    ["remove_docker,doc_link"]=""
+)
+#
+# @decription remove docker
+#
+remove_docker ()
+{
+	if check_if_installed docker-ce; then
+    debconf-apt-progress -- apt-get autoremove --purge -y docker-ce
+  else
+    debconf-apt-progress -- apt-get autoremove --purge -y docker.io
   fi
 }
